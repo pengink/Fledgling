577980c2128c3ce185f9f2a88bba51220f8fb582
pengink <150305134+pengink@users.noreply.github.com>
2024-02-05 15:19:42 -0800

    Hotbar Layout

---
+++ Fledgling/HotbarController.gd
@@ -0,0 +1,16 @@
+extends Container
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var camera = Camera2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_Player_tree_entered(player):
+	pass
+	
