7984a5e8d0ebd83b3e1d74026593925c4553a120
pengink <150305134+pengink@users.noreply.github.com>
2024-02-05 15:24:03 -0800

    Hotbar v2

--- Fledgling/HotbarController.gd
+++ Fledgling/HotbarController.gd
@@ -5,12 +5,23 @@ extends Container
 # var a = 2
 # var b = "text"
 var camera = Camera2D
+var scroll_index = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func _on_Player_tree_entered(player):
+func _input(event):
 	pass
-	
+	if Input.is_action_just_pressed("scroll_down"):
+		scroll_index += 1
+	if Input.is_action_just_pressed("scroll_up"):
+		pass
+		scroll_index += 1
+		
+	if scroll_index > 31 or scroll_index < 0:
+		pass
+		
+	scroll_index = clamp(scroll_index,0, 30)
+		
+		
