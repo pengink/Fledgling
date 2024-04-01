bb82eaca536114f6ba34e310344bc2f6956c071f
pengink <150305134+pengink@users.noreply.github.com>
2024-03-31 23:21:45 -0700

    Revert "character animation and lighting"

--- Fledgling/Player.gd
+++ Fledgling/Player.gd
@@ -5,11 +5,7 @@ var velocity : Vector2
 var jumped : bool = false
 var gliding : bool = false
 
-var state = ["idle", "running", "jumping", "gliding"]
-export var currentState = "idle"
-
 onready var collider = $Area2D
-onready var animatedSprite = get_node("AnimatedSprite")
 
 export var jump_height : float = 100.0
 export var time_to_peak : float = 3.0
@@ -34,27 +30,18 @@ func _process(delta):
 		else:
 			glide()
 			
-	if gliding == false or get_vector_x() == 0:
+	if gliding == false or  get_vector_x() == 0:
 		velocity.y += get_gravity() * delta
-		if get_gravity() == jump_gravity:
-			currentState = "Jumping"
 
 	if gliding == true and get_vector_x() != 0:
-		velocity.y = lerp(velocity.y, glide_velocity, delta * glide_weight) # WHY
+			velocity.y = lerp(velocity.y, glide_velocity, delta * glide_weight) # WHY
 		
 	move_and_slide(velocity, Vector2.UP) # applies overall movement
-	animationState()
-
-	
 	
 func _input(event):
 	if is_on_floor(): 
 		jumped = false
 		gliding = false
-	if get_vector_x() > 0:
-		animatedSprite.flip_h = true
-	elif get_vector_x() < 0:
-		animatedSprite.flip_h = false
 		
 func get_vector_x():
 	return Input.get_action_raw_strength("Right") - Input.get_action_raw_strength("Left") # get movement on x axis
@@ -82,33 +69,3 @@ func pickup(item):
 	pass
 	item.get_parent().remove_child(item)
 	print("krill")
-	
-func animationState():
-	if is_on_floor() == true:
-		if get_vector_x() == 0:
-			currentState = "idle"
-		elif get_vector_x() != 0:
-			currentState = "running"
-	else:
-		if jumped == true && gliding == false:
-			currentState = "jumping"
-		elif gliding == true:
-			currentState = "GLIDING"
-			
-	animation()
-
-func animation():
-	match currentState:
-		"GLIDING":
-			animatedSprite.play("GLIDING")
-			animatedSprite.set_scale(Vector2(1, 1))
-		"running":
-			animatedSprite.play("running")
-			animatedSprite.set_scale(Vector2(0.1, 0.1))
-		"jumping":
-			animatedSprite.play("Jumping")
-			animatedSprite.set_scale(Vector2(1, 1))
-		"idle":
-			animatedSprite.play("Jumping")
-			animatedSprite.set_scale(Vector2(1, 1))
-	
